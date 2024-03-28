6940533499678d22795097277cace412895ce766
Brian Wettstone <wettstbe@dukes.jmu.edu>
2024-03-28 14:23:30 -0400

    final

---
+++ scripts/createcard.js
@@ -0,0 +1,8 @@
+let pb = document.getElementById("pb")
+let sb = document.getElementById("sb")
+if(localStorage.getItem("cards")) { 
+    let cards = localStorage.getItem("cards");
+} else {
+    
+    let cards = [];
+}
